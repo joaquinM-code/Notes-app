d457df7cf42e155c0ea01b679e115a390a773447
ecatom <netpassrecovers@hotmail.com>
2019-12-25 21:21:05 +0100

    25/12/19

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,37 +1,24 @@
-const request = require('request');
 const geocode = require('./utils/geocode.js')
-// const url = 'https://api.darksky.net/forecast/777ce1bf03ae3e561251ee4dbfe90f3f/41.2101,-2.0138?units=si';
-
-// request({ url:url, json:true}, (error, response)=>{
-//     if(error){
-//         console.log('Problem connecting to the weather service!!')
-//     }else if(response.body.error){
-//         console.log('Unable to find location')
-//     }else{
-//         const currentlyData = response.body.currently
-//         console.log(response.body.daily.data[0].summary+' '+currentlyData.temperature+'C. There is a '+currentlyData.precipProbability+'% chance of rain.')
-//     }
-// });
+const forecast = require('./utils/forecast.js')
 
 
-// const urlGeo='https://api.mapbox.com/geocoding/v5/mapbox.places/sant%20boi%20llobregat.json?access_token=pk.eyJ1IjoiZWNhdG9tIiwiYSI6ImNrNGI1YzNnYTBhYmYzZW5pY2thaXFtMGYifQ.aTSj5OZYI1jenUD0HKK1hg&limit=1'
-
-// request({url:urlGeo,json:true},(error, response)=>{
-//     if(error){
-//         console.log('Problem connecting to the location service!!')
-//     }else if(!response.body.features){
-//         console.log('Unable to retrive location data')
-//     }else{
-//         const latitude = response.body.features[0].center[1]
-//         const longitude = response.body.features[0].center[0]
-//         console.log(latitude, longitude)
-//     }
-    
-// })
-
-
-
-geocode('Boston', (error, data)=>{
-    console.log('Error',error)
-    console.log('DaTA',data)
-})
+const location = process.argv[2]
+if(!location){
+    console.log('Please, provide a city name')
+}else if(process.argv[3]){
+    console.log('Provide the address inside double quotes')
+}else{
+    geocode(location, (error, {latitude , longitude , location}={})=>{
+        if(error){
+            return console.log('Error',error);
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error){
+                return console.log('Error', error)
+            }
+            console.log(location);
+            console.log(forecastData)
+            
+          })
+    })
+}
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -3,16 +3,16 @@ const request = require('request')
 const geocode = (address, callback)=>{
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token=pk.eyJ1IjoiZWNhdG9tIiwiYSI6ImNrNGI1YzNnYTBhYmYzZW5pY2thaXFtMGYifQ.aTSj5OZYI1jenUD0HKK1hg&limit=1';
 
-    request({url, json: true}, (error, response)=>{
+    request({url, json: true}, (error, {body:{features}={}}={})=>{
         if(error){
             callback('Problem connecting to the location service!!', undefined);
-        }else if(!response.body.features){
+        }else if(!features){
             callback('Unable to retrive location data', undefined)
         }else{
             callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name 
+                latitude: features[0].center[1],
+                longitude: features[0].center[0],
+                location: features[0].place_name 
             })
         }
     })
